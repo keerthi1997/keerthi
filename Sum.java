97ba408ef12b0c0a8c8ca5d3477ec08df6a17f4a
keerthi1997 <keerthana.vjps@gmail.com>
2017-08-05 09:25:49 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Sum{
+  public static void main(String[] args)throws IOException
+  {
+  Scanner sc=new Scanner(System.in);
+  int n,i,temp=0;
+  n=sc.nextInt();
+  for(i=1;i<=n;i++)
+  {
+    temp=temp+i;
+    }
+    System.out.println(temp);
+    }
+    }
+  
+  
