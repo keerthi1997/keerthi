0dc56bf9826d8153950b56a1b4326d5c93c99cd9
keerthi1997 <keerthana.vjps@gmail.com>
2017-08-05 09:36:00 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Concat
+{
+  public static void main(String[] args)throws IOException
+  {
+      String s,s1;
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      s=br.readLine();
+      s1=br.readLine();
+      System.out.println(s+s1);
+      }
+      }
